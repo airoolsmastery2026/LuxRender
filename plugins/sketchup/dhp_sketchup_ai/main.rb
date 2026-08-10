ac91b4f51b57826f74537f96a3a7f0d217d7be6b
HUU HUONG NGUYEN <airoolsmastery2026@gmail.com>
2026-08-10 09:18:35 +0700

    feat: add LuxRender-aware SketchUp bootstrap

---
+++ plugins/sketchup/dhp_sketchup_ai/main.rb
@@ -0,0 +1,35 @@
+require 'sketchup.rb'
+require 'json'
+require_relative 'core/model_service'
+require_relative 'core/dialog_service'
+require_relative 'core/server_service'
+
+module DaiHaiPhat
+  module SketchUpAI
+    DEFAULT_APP_URL = 'http://127.0.0.1:3000'.freeze
+
+    def self.app_url
+      Sketchup.read_default(EXTENSION_ID, 'app_url', DEFAULT_APP_URL).to_s
+    end
+
+    def self.app_url=(value)
+      Sketchup.write_default(EXTENSION_ID, 'app_url', value.to_s)
+    end
+
+    def self.show
+      DialogService.show
+    end
+
+    unless file_loaded?(__FILE__)
+      cmd = UI::Command.new('DHP SketchUp AI') { show }
+      cmd.tooltip = 'Open LuxRender SketchUp adapter'
+      cmd.status_bar_text = 'Open DHP SketchUp AI / LuxRender panel'
+      UI.menu('Extensions').add_item(cmd)
+      UI.menu('Extensions').add_item('Open LuxRender') { ServerService.launch_app }
+      toolbar = UI::Toolbar.new('DHP SketchUp AI')
+      toolbar.add_item(cmd)
+      toolbar.restore
+      file_loaded(__FILE__)
+    end
+  end
+end
